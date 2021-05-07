bec2cfa70ced9f7b61db5a1df81d4ac26153d30b
JINIL <il633990@gmail.com>
2021-05-07 20:26:39 +0900

    Study and post the answer to the problem.

--- leetcode/Week2/Reach_a_number/JI_Solution.java
+++ leetcode/Week2/Reach_a_number/JI_Solution.java
@@ -2,7 +2,14 @@ package leetcode.Week2.Reach_a_number;
 
 public class JI_Solution {
     public int reachNumber(int target) {
-        // TODO
-        return 1;
+        target = Math.abs(target);
+        int k = 0;
+        while (target > 0)
+            target -= ++k;
+
+        if (target % 2 == 0)
+            return k;
+
+        return k + 1 + k % 2;
     }
 }
