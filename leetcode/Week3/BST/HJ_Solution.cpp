001465ae7762f1dc5cf6b2070b1713da71d4d7ea
hj-d <star8589@gmail.com>
2021-05-15 13:37:51 +0900

    Solve week3 problem
    I solved only 2 problem.
    They are just BST problem.
    
    Signed-off-by: hj-d <star8589@gmail.com>

--- leetcode/Week3/BST/HJ_Solution.cpp
+++ leetcode/Week3/BST/HJ_Solution.cpp
@@ -9,9 +9,31 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+int arr[10000];
+int idx = 0;
+void search(TreeNode* head){
+    if (head == nullptr) {
+        return;
+    }
+    arr[idx++] = head->val;
+    search(head->left);
+    search(head->right);
+}
+
 class Solution {
 public:
     int getMinimumDifference(TreeNode* root) {
-        
+        idx = 0;
+        search(root);
+        sort(arr, arr+idx);
+        int min = 100000;
+        int tmp;
+        for (int i = 0; i < idx - 1; ++i) {
+            tmp = arr[i+1] - arr[i];
+            if (min > tmp)
+                min = tmp;
+        }
+        return min;
     }
 };
--- leetcode/Week3/BTPT/HJ_Solution.cpp
+++ leetcode/Week3/BTPT/HJ_Solution.cpp
@@ -9,9 +9,22 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+vector<int> ans;
+
+void search(TreeNode* head){
+    if (head == nullptr) {
+        return;
+    }
+    search(head->left);
+    search(head->right);
+    ans.push_back(head->val);
+}
+
 class Solution {
 public:
     vector<int> postorderTraversal(TreeNode* root) {
-        
+        ans.clear();
+        search(root);
+        return ans;        
     }
 };
